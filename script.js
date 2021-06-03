9449f008c94e21c0514e6eed017f21dd15389373
LRomero2 <lauren.romero@outlook.com>
2021-06-03 21:49:05 +0000

    Added the extra code from the codepen and trying to get the code working with the new way to get the current date. Working in codepen but not working here yet.

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
-$("#trigger").ready(function(){
-  $("button").click(function(){
+// This is a little function that does just one thing.
+// This is clean code :-)
+function createListItem(daysUntil, text){
+  return `<li>${daysUntil} ${text}</li>`;
+}
+// How many places would we have to change the code if we wanted to:
+// - change the way got todays date?
+// - add a smiley face to every list item?
+
+const showSmile = function() {
+  return ':-)';
+}
+// This populates the day until when the button is clicked
+$("#trigger1").click(function(){
+  const holidayDates = [
+    [new Date(2021,11,25), ('days until Christmas Day')], 
+    [new Date(2022,0,01), ('days until New Years Day')], 
+    [new Date(2022,1,14), ('days until Valentines Day')]];
   
+
+var today = new Date();
+
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+
+  holidayDates.forEach((item) => 
+  {const calcDaysUntil = Math.ceil((item[0].getTime() - today.getTime()) / (1000 * 60 * 60 * 24));
+   
+   const countdownResult = createListItem(calcDaysUntil, item[1]);
+    $("#days-until").append(countdownResult);
+   holidayDates.push(':-)');
+  });
+})
+
+// This populates the days until when the mouse hovers over the image
+$("#trigger2").mouseover(function(){
   const holidayDates = [
     [new Date(2021,11,25), ('days until Christmas Day')], 
     [new Date(2022,0,01), ('days until New Years Day')], 
@@ -11,9 +43,9 @@ $("#trigger").ready(function(){
   holidayDates.forEach((item) => 
   {const calcDaysUntil = Math.ceil((item[0].getTime() - today.getTime()) / (1000 * 60 * 60 * 24));
    
-    const countdownResult = `<li> ${calcDaysUntil} ${item[1]}</li>`;
+   const countdownResult = createListItem(calcDaysUntil, item[1]);
     $("#days-until").append(countdownResult);
   });
-});
-
 })
+
+
