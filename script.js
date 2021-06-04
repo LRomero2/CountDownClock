f5cb08a410c473016fcecd2cd676669e82c95ddb
LRomero2 <lauren.romero@outlook.com>
2021-06-04 10:52:50 +0000

    Added the smiley face function and appended it to display after each date and holiday text. Also changed the way to get todays date.

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ function createListItem(daysUntil, text){
 // How many places would we have to change the code if we wanted to:
 // - change the way got todays date?
 // - add a smiley face to every list item?
-
 const showSmile = function() {
   return ':-)';
 }
@@ -16,18 +15,16 @@ $("#trigger1").click(function(){
     [new Date(2021,11,25), ('days until Christmas Day')], 
     [new Date(2022,0,01), ('days until New Years Day')], 
     [new Date(2022,1,14), ('days until Valentines Day')]];
-  
 
 var today = new Date();
 
-var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-
+var todaysDate = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+  
   holidayDates.forEach((item) => 
   {const calcDaysUntil = Math.ceil((item[0].getTime() - today.getTime()) / (1000 * 60 * 60 * 24));
    
    const countdownResult = createListItem(calcDaysUntil, item[1]);
-    $("#days-until").append(countdownResult);
-   holidayDates.push(':-)');
+    $("#days-until").append(countdownResult).append(showSmile);
   });
 })
 
@@ -38,14 +35,14 @@ $("#trigger2").mouseover(function(){
     [new Date(2022,0,01), ('days until New Years Day')], 
     [new Date(2022,1,14), ('days until Valentines Day')]];
 
-  const today = new Date();
+var today = new Date();
+
+var todaysDate = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
   
   holidayDates.forEach((item) => 
   {const calcDaysUntil = Math.ceil((item[0].getTime() - today.getTime()) / (1000 * 60 * 60 * 24));
    
    const countdownResult = createListItem(calcDaysUntil, item[1]);
-    $("#days-until").append(countdownResult);
+     $("#days-until").append(countdownResult).append(showSmile);
   });
 })
-
-
